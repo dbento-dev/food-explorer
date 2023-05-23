128684a14819c0a82976d431316e2c3494074f8e
David Bento <dbento.dev@gmail.com>
2023-05-23 20:58:03 -0300

    start frontend food explorer

---
+++ src/styles/global.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+
+    -webkit-font-smoothing: antialiased;
+
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);    
+  }
+`
