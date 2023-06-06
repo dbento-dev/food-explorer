71429583c8bb12074493a6a369fde7a3070a749f
David Bento <dbento.dev@gmail.com>
2023-06-06 14:35:46 -0300

    teste github

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,12 @@ import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
 import { Details } from './pages/Details'
-// import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Details />
-      {/* <Home /> */}
     </ThemeProvider>
   </React.StrictMode>
 )
