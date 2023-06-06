7809390925f11bdc00e486b25f9df5d6e4402498
David Bento <dbento.dev@gmail.com>
2023-06-06 19:14:58 -0300

    feat: signIn page

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { Background, Container, Form } from './styles'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+export function SignIn() {
+  // eslint-disable-next-line no-unused-vars
+  const [email, setEmail] = useState('')
+  // eslint-disable-next-line no-unused-vars
+  const [password, setPassword] = useState('')
+
+  const handleSignIn = () => {
+    console.log('handleSignIn')
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <div>
+          <h1>Faça login</h1>
+          <label htmlFor="email">Email</label>
+          <Input
+            name="email"
+            placeholder="exemplo@exemplo.com.br"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="password">Senha</label>
+          <Input
+            name="password"
+            placeholder="No mínimo 6 caracteres"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button title="Entrar" onClick={handleSignIn} />
+
+          <a to="/register">Criar uma conta</a>
+        </div>
+      </Form>
+    </Container>
+  )
+}
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+
+import foodExplorerLogoSVG from '../../assets/food-explorer-logo.svg'
+import { device } from '../../styles/theme'
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1.6rem;
+
+  @media ${device.laptop} {
+    flex-direction: row;
+    flex-direction: row;
+    align-items: stretch;
+    gap: 0;
+  }
+`
+
+export const Background = styled.div`
+  background: url(${foodExplorerLogoSVG}) no-repeat center center;
+  height: 10rem;
+
+  @media ${device.laptop} {
+    flex: 1;
+    height: 100vh;
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 4.7rem;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+    height: 100%;
+    padding: 6.4rem;
+    border-radius: 1.6rem;
+
+    > h1 {
+      font-size: 2.2rem;
+      font-weight: 500;
+      color: ${({ theme }) => theme.COLORS.LIGHT_100};
+      align-self: center;
+    }
+
+    > label {
+      font-size: 1.6rem;
+      font-weight: 400;
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
+      margin-top: 3.2rem;
+      margin-bottom: 0.8rem;
+    }
+
+    > button {
+      margin-top: 3.2rem;
+    }
+
+    > a {
+      margin-top: 3.2rem;
+      color: ${({ theme }) => theme.COLORS.LIGHT_100};
+      align-self: center;
+    }
+  }
+
+  @media ${device.laptop} {
+    > div {
+      height: 54rem;
+
+      > h1 {
+        font-size: 3.2rem;
+      }
+    }
+  }
+`
