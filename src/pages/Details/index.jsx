4796af8dec50d1cd12191fde05272bb5d94daf8f
David Bento <dbento.dev@gmail.com>
2023-06-29 19:50:02 -0300

    feat: get recipe details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,61 +1,84 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { RxCaretLeft, RxMinus, RxPlus } from 'react-icons/rx'
+
+import { Container, Content } from './styles'
+
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 import { ButtonText } from '../../components/ButtonText'
 import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
-import { RxCaretLeft, RxMinus, RxPlus } from 'react-icons/rx'
 
-import { Container, Content } from './styles'
+import { useAuth } from '../../hooks/auth'
+import { api } from '../../services/api'
+
+import { generateImageUrl } from '../../helpers/helpers'
 
 export function Details() {
+  const { isAdmin } = useAuth()
+
+  const params = useParams()
+
+  const [data, setData] = useState({})
+
+  const navigate = useNavigate()
+
+  const handleEditRecipe = (id) => {
+    navigate(`/edit/${id}`)
+  }
+
+  useEffect(() => {
+    async function getDishDetail() {
+      const response = await api.get(`/recipes/${params.id}`)
+
+      setData(response.data)
+    }
+
+    getDishDetail()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <Container>
-      <Header />
+      <Header isAdmin={isAdmin} />
       <main>
         <Content>
           <ButtonText to="/" title="voltar" icon={RxCaretLeft} />
           <div className="dish">
             <div className="dishImage">
               <img
-                src="https://source.unsplash.com/265x265/?food"
+                src={generateImageUrl(data?.image)}
                 alt="Imagem do prato selecionado"
               />
             </div>
 
             <div className="dishDetails">
-              <h2>Salada Ravanello</h2>
-              <p>
-                Rabanetes, folhas verdes e molho agridoce salpicados com
-                gergelim.
-              </p>
+              <h2>{data?.name}</h2>
+              <p>{data?.description}</p>
               <div>
-                <Tag title="cebola" />
-                <Tag title="alface" />
-                <Tag title="açucar" />
-                <Tag title="sal" />
-                <Tag title="leite" />
-                <Tag title="arroz" />
-                <Tag title="feijão" />
-                <Tag title="ovos" />
-                <Tag title="maçã" />
-                <Tag title="tomate" />
-                <Tag title="cenoura" />
-                <Tag title="manteiga" />
-                <Tag title="pimentão" />
-                <Tag title="couve" />
-                <Tag title="couve-flor" />
-                <Tag title="cravo" />
-                <Tag title="batata-doce" />
-                <Tag title="farinha" />
+                {data?.ingredients?.map((ingredient) => {
+                  const { id, name } = ingredient
+                  return <Tag key={id} title={name} />
+                })}
               </div>
 
-              <div>
-                <RxMinus />
-                <span>01</span>
-                <RxPlus />
+              {!isAdmin ? (
+                <div>
+                  <RxMinus />
+                  <span>01</span>
+                  <RxPlus />
 
-                <Button title={`incluir ∙ R$ 49,30`} />
-              </div>
+                  <Button title={`incluir ∙ R$ ${data?.price}`} />
+                </div>
+              ) : (
+                <div>
+                  <Button
+                    title="Editar prato"
+                    onClick={() => handleEditRecipe(data?.id)}
+                  />
+                </div>
+              )}
             </div>
           </div>
         </Content>
