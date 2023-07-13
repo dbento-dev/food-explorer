cd851d7ab1aa44dff8bb74ab4c9c5d067f48b7b4
David Bento <dbento.dev@gmail.com>
2023-07-13 19:01:42 -0300

    feat: add loading state at add new recipe

--- src/pages/FormDish/index.jsx
+++ src/pages/FormDish/index.jsx
@@ -19,6 +19,7 @@ import { CurrencyInput } from '../../components/CurrencyInput'
 import avatarPlaceholderPng from '../../assets/upload-placeholder.png'
 import { useNavigate } from 'react-router-dom'
 import { api } from '../../services/api'
+import { Spinner } from '../../components/Spinner'
 
 export function FormDish() {
   const navigate = useNavigate()
@@ -40,6 +41,7 @@ export function FormDish() {
   const [avatar, setAvatar] = useState(imageURL)
   // eslint-disable-next-line no-unused-vars
   const [avatarFile, setAvatarFile] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const disabledSubmitButton =
     !avatarFile || !name || !category || !price || !description
@@ -75,6 +77,8 @@ export function FormDish() {
       return
     }
 
+    setIsLoading(true)
+
     try {
       if (avatarFile) {
         const formData = new FormData()
@@ -92,8 +96,10 @@ export function FormDish() {
 
         alert('Prato cadastrado com sucesso!')
         navigate('/')
+        setIsLoading(false)
       }
     } catch (error) {
+      setIsLoading(false)
       if (error.response) {
         alert(error.response.data.message)
       } else {
@@ -122,106 +128,110 @@ export function FormDish() {
       <Header />
 
       <main>
-        <Content>
-          <ButtonText to="/" title="voltar" icon={RxCaretLeft} />
-          <h2>Adicionar prato</h2>
+        {!isLoading && (
+          <Content>
+            <ButtonText to="/" title="voltar" icon={RxCaretLeft} />
+            <h2>Adicionar prato</h2>
 
-          <Form id="form-dish" name="form-dish">
-            <div className="row">
-              <div className="form-group dish-photo">
-                <span>Imagem do prato</span>
-                <Avatar>
-                  <img src={avatar} alt="Foto do prato" />
+            <Form id="form-dish" name="form-dish">
+              <div className="row">
+                <div className="form-group dish-photo">
+                  <span>Imagem do prato</span>
+                  <Avatar>
+                    <img src={avatar} alt="Foto do prato" />
 
-                  <label htmlFor="dish-photo">
-                    <FiCamera />
-                    <input
-                      id="dish-photo"
-                      name="dish-photo"
-                      type="file"
-                      accept="image/*"
-                      onChange={handleChangeAvatar}
-                    />
-                  </label>
-                </Avatar>
-              </div>
-              <div className="form-group name">
-                <label htmlFor="name">Nome</label>
-                <Input
-                  name="name"
-                  placeholder="Ex.: Salada Ceasar"
-                  type="text"
-                  onChange={(e) => setName(e.target.value)}
-                  value={name}
-                />
-              </div>
-              <div className="form-group category">
-                <label htmlFor="category">Categoria</label>
-                <Select
-                  name="category"
-                  id="category"
-                  onChange={(e) => setCategory(e.target.value)}
-                  value={category}
-                >
-                  <option value="">Selecione</option>
-                  <option value="drink">Bebida</option>
-                  <option value="dish">Refeição</option>
-                  <option value="dessert">Sobremesa</option>
-                </Select>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="form-group ingredients-list">
-                <div className="ingredients-list">
-                  <span>Ingredientes</span>
-                  <div className="ingredients">
-                    {ingredients.map((ingredient, index) => (
-                      <IngredientsItem
-                        key={String(index)}
-                        isNew={false}
-                        value={ingredient}
-                        onClick={() => {
-                          handleRemoveIngredient(ingredient)
-                        }}
+                    <label htmlFor="dish-photo">
+                      <FiCamera />
+                      <input
+                        id="dish-photo"
+                        name="dish-photo"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleChangeAvatar}
                       />
-                    ))}
-
-                    <IngredientsItem
-                      isNew
-                      placeholder="Novo ingrediente"
-                      value={newIngredient}
-                      onChange={(e) => setNewIngredient(e.target.value)}
-                      onClick={handleAddIngredient}
-                    />
-                  </div>
+                    </label>
+                  </Avatar>
+                </div>
+                <div className="form-group name">
+                  <label htmlFor="name">Nome</label>
+                  <Input
+                    name="name"
+                    placeholder="Ex.: Salada Ceasar"
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                  />
+                </div>
+                <div className="form-group category">
+                  <label htmlFor="category">Categoria</label>
+                  <Select
+                    name="category"
+                    id="category"
+                    onChange={(e) => setCategory(e.target.value)}
+                    value={category}
+                  >
+                    <option value="">Selecione</option>
+                    <option value="drink">Bebida</option>
+                    <option value="dish">Refeição</option>
+                    <option value="dessert">Sobremesa</option>
+                  </Select>
                 </div>
               </div>
 
-              <div className="form-group price">
-                <label htmlFor="price">Preço</label>
-                <CurrencyInput setValue={setPrice} value={price} />
-              </div>
-            </div>
+              <div className="row">
+                <div className="form-group ingredients-list">
+                  <div className="ingredients-list">
+                    <span>Ingredientes</span>
+                    <div className="ingredients">
+                      {ingredients.map((ingredient, index) => (
+                        <IngredientsItem
+                          key={String(index)}
+                          isNew={false}
+                          value={ingredient}
+                          onClick={() => {
+                            handleRemoveIngredient(ingredient)
+                          }}
+                        />
+                      ))}
 
-            <div className="row">
-              <div className="form-group description">
-                <TextArea
-                  placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
-                  label="Descrição"
-                  onChange={(e) => setDescription(e.target.value)}
-                  value={description}
-                />
-              </div>
-            </div>
+                      <IngredientsItem
+                        isNew
+                        placeholder="Novo ingrediente"
+                        value={newIngredient}
+                        onChange={(e) => setNewIngredient(e.target.value)}
+                        onClick={handleAddIngredient}
+                      />
+                    </div>
+                  </div>
+                </div>
 
-            <Button
-              title="Adicionar prato"
-              onClick={handleNewDish}
-              disabled={disabledSubmitButton}
-            />
-          </Form>
-        </Content>
+                <div className="form-group price">
+                  <label htmlFor="price">Preço</label>
+                  <CurrencyInput setValue={setPrice} value={price} />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="form-group description">
+                  <TextArea
+                    placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+                    label="Descrição"
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                  />
+                </div>
+              </div>
+
+              <Button
+                title="Adicionar prato"
+                onClick={handleNewDish}
+                disabled={disabledSubmitButton}
+              />
+            </Form>
+          </Content>
+        )}
+
+        {isLoading && <Spinner />}
       </main>
       <Footer />
     </Container>
