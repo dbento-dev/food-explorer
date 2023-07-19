9ca4794f4eed68752e01f0aa870515f87429f0d8
David Bento <dbento.dev@gmail.com>
2023-07-19 16:51:12 -0300

    feat: footer test component

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -4,8 +4,12 @@ import { Footer } from '.'
 
 describe('Footer', () => {
   it('should render', () => {
-    const { getByText } = render(<Footer />)
+    const { getByText, getByAltText } = render(<Footer />)
 
+    expect(getByAltText('logo do food explorer')).toBeInTheDocument()
     expect(getByText('food explorer')).toBeInTheDocument()
+    expect(
+      getByText('© 2023 - Todos os direitos reservados.')
+    ).toBeInTheDocument()
   })
 })
