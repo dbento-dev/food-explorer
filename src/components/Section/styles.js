83674bc3cc47d1708d16c3815947e249827cc3fe
David Bento <dbento.dev@gmail.com>
2023-07-25 20:23:45 -0300

    fix: add device responsiveness at section component

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { device } from '../../styles/theme'
 
 export const Container = styled.section`
   margin: 3.6rem 0 2.8rem;
@@ -7,7 +8,13 @@ export const Container = styled.section`
     font-size: 1.8rem;
     font-weight: 500;
     color: ${({ theme }) => theme.COLORS.LIGHT_300};
-
     margin-bottom: 2.4rem;
+    font-style: normal;
+  }
+
+  @media ${device.laptop} {
+    > h2 {
+      font-size: 3.2rem;
+    }
   }
 `
